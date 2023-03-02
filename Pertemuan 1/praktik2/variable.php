80d5f7693db9cf19a90ad732e411c89b5499492c
Gippsyone999 <mhmmadgifari.maulidi77@gmail.com>
2023-03-02 15:55:54 +0700

    Peraktikum 1

---
+++ Pertemuan 1/praktik2/variable.php
@@ -0,0 +1,22 @@
+<?php
+
+    /*
+        Varibale sistem
+        Variable built in php
+    */
+    echo $_SERVER["DOCUMENT_ROOT"];
+
+
+
+    /*
+        Variable user
+        Vriable yang dibuat oleh user
+    */
+    echo '<br><br>';
+
+    $name = 'Gip';
+    $name = 'Gipari';
+
+    echo 'Nama saya adalah ' . $name;
+
+?>
